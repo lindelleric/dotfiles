bf0086097f2ed1bcb2f6bd9dcbd6fc7a04dae9b1
Eric Lindell <eric@lindell.id>
2025-02-19 15:13:46 +0100

    nvim: add no neck pain plugin

---
+++ nvim-carbon/.config/nvim/lua/plugins/no_neck_pain.lua
@@ -0,0 +1,8 @@
+return { -- Autoformat
+  'shortcuts/no-neck-pain.nvim',
+  version = '*',
+  keys = {
+    { '<leader>cc', ':NoNeckPain<CR>', desc = 'Toggle no neck pain', silent = true },
+  },
+  opts = {},
+}
